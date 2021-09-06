52cc29a02c80cd7c7a2cb1b0b49d1e9eb3c29919
Brad Bell <bradbell@seanet.com>
2021-09-06 03:46:17 -0700

    main: test cmake/compile_source_test.cmake on macos-latest.

---
+++ cmake/compile_source_test.cmake
@@ -0,0 +1,33 @@
+INCLUDE(CheckCXXSourceCompiles)
+MACRO(compile_source_test source variable)
+    #
+    # check that variable is not yet defined
+    IF( DEFINED ${variable} )
+        MESSAGE(FATAL_ERROR
+            "compile_source_test: ${variable} is defined before expected"
+        )
+    ENDIF( DEFINED ${variable} )
+    #
+    IF( DEFINED compiles_source_test_result)
+        UNSET(compiles_source_test_result)
+    ENDIF( DEFINED compiles_source_test_result )
+    #
+    # check that source codee compiles
+    CHECK_CXX_SOURCE_COMPILES("${source}" compiles_source_test_result )
+    #
+    # change result varialbe to 0 (1) for fail (succeed).
+    IF( compiles_source_test_result )
+        SET(${variable} 1)
+    ELSE( compiles_source_test_result )
+        SET(${variable} 0)
+    ENDIF( compiles_source_test_result )
+    #
+    # check that varialbe is defined
+    IF( NOT DEFINED ${variable} )
+        MESSAGE(FATAL_ERROR
+            "compile_source_test: error in CMake script."
+        )
+    ENDIF( NOT DEFINED ${variable} )
+    #
+    MESSAGE(STATUS "${variable} = ${${variable}}" )
+ENDMACRO( compile_source_test )
